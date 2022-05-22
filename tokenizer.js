27e2a69c698a440750b1b8513e6feb5f88ecf88a
Vilho Luoma <vilho.luoma@gmail.com>
2022-05-23 01:32:09 +0300

    refactor most of the stuff, test

--- tokenizer.js
+++ tokenizer.js
@@ -10,15 +10,13 @@ const Spec = [
 ];
 
 class Tokenizer {
-
-
     init(string) {
         this.string = string;
         this.cursor = -1;
         this.current_char = null;
         this.token = new Token();
+        this.tokens = [];
         this.advance();
-        this.isKeySet = false;
     }
 
     advance() {
@@ -39,6 +37,10 @@ class Tokenizer {
         return this.cursor === this.string.length;
     }
 
+    peek(offset = 1) {
+        return this.string[this.cursor + offset];
+    }
+
     readNumber() {
         // check if number
         if(!Number.isNaN(this.current_char)) {
@@ -52,33 +54,6 @@ class Tokenizer {
         }
     }
 
-    readNext() {
-        while(!this.isEOF() && !g.isCarriageReturn(this.current_char)) {
-            
-        }
-    }
-    
-
-    readWhile(closingTag) {
-        let string = '';
-        
-            while (!this.isEOF() && this.current_char !== closingTag) {
-                let c = String.fromCharCode(this.current_char);
-                if(this.current_char === closingTag) {
-                    return string;
-                }
-    
-                if(g.isUnquotedKey(this.current_char) 
-                    || g.isDotSep(this.current_char)) {
-                    string += c;
-                }
-    
-                this.advance();
-            }
-            return string;
-        }
-    // }
-
     readString() {
         let s = '';        
         while(!this.isEOF() && !g.isQuotationMark(this.current_char)) {
@@ -89,83 +64,66 @@ class Tokenizer {
         return s;
     }
 
+    readKey() {
+        const val = this.readWhile(g.isUnquotedKey());
+        return val;
+    }
+
+    readPunc() {
+        const val = String.fromCodePoint(this.next());
+        console.log(val)
+        return val;
+    }
+
+    readNext() {
+        const x = this.readWhile((c) => g.isCarriageReturn(c) || g.isNewline(c) || g.isWhitespace(c));
+        if (this.isEOF()) {
+            return null;
+        }
+        let c = this.current_char;
+        // if (g.isCommentStart(c)) {
+        //     readComment();
+        //     return null;
+        // }
+        // if (g.isDigit(c)) {
+        //     return readNumber();
+        // }
+        if (g.isUnquotedKey(c)) {
+            return this.readKey();
+        }
+        if (g.isKeyValSep(c)) {
+            return this.readPunc();
+        }
+        if (g.isQuotationMark(c)) {
+            return this.readString();
+        }
+        throw new SyntaxError(`Unable to read next token: ${c} not supported.`);
+    }
+    
+
+    readWhile(condition) {
+        let s = '';
+        while (!this.isEOF() && !condition(this.current_char)) {
+            let char = String.fromCharCode(this.current_char);
+            s += char;
+            this.advance();
+        }
+        return s;
+    }
+
+    next() {
+        return this.current_char || this.readNext();
+    }
+
     tokenize() {
         let tokens = [];
-        let s = '';
-
-        while (this.current_char !== null && !this.isEOF()) {
-            const next = this.peek(1);
-            let c = String.fromCharCode(this.current_char);
-           
-            if(g.isWhitespace(this.current_char)) {
-                this.advance();
-            }
-
-            else if(g.isUnquotedKey(this.current_char)) {
-                s += c;
-                this.advance();
-            }  
-
-            else if(next === undefined) {
-                tokens.push(this.token.copy())
-                this.advance();
-            }
-  
-            else if(g.isOpeningSquareBracket(this.current_char)){
-                const value = this.readWhile(93); // closing square bracket
-                this.token.key = value;
-                s = '';
-                // if(value.match(/package/i) !== null) {
-                //     this.token.key = value;
-                //     tokens.push(this.token.copy())
-                // } 
-                this.advance();
-            }  
-
-            // prevent from copying the previous line
-            else if(g.isNewline(this.current_char) && !g.isCarriageReturn(next)) {
-                const token = this.token.copy();
-                tokens.push(token);
-                this.advance();
-            }
-
-            else if(g.isKeyValSep(this.current_char)){
-                this.token.key = s;
-                s = '';
-                this.advance();
-            }
-
-            else if(g.isQuotationMark(this.current_char)) {
-                this.advance();
-                const value = this.readString(); // "
-                this.token.value = value;
-            }
-
-            else {
-                this.advance();
-            }          
+        while(this.hasMoreTokens) {
+            const x = this.readNext();
+            tokens.push(x);
+            this.advance();
         }
-
         return tokens;
     }
-
-    // const x = this.match(/\[[a-zA-Z]+\]/, value);
-    // match(regexp, string) {
-    //     const matched = regexp.exec(string);
-    //     if(matched == null) {
-    //         return null;
-    //     } 
-    //     console.log(matched)
-    //     return matched[0];
-    // }
-
-
-    peek(offset = 0) {
-        return this.string[this.cursor + offset];
-    }
-
-
-
 }
 
 module.exports = {
