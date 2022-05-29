2bb587cc62cf68355fd24ebbf6ad8421afe505d9
Vilho Luoma <vilho.luoma@gmail.com>
2022-05-30 02:59:55 +0300

    second commit

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/v1', routes);
 
-if(process.env.NODE_ENV === "production") {
+if(process.env.NODE_ENV !== "production") {
     app.use(express.static('build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, '../../../', 'frontend', 'build', 'index.html'))
