d961680ec1eac51e499e932206256a0bba50024a
Vilho Luoma <vilho.luoma@gmail.com>
2022-05-30 09:18:13 +0300

    deployment

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/v1', routes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static('build'));
+if(process.env.NODE_ENV !== "production") {
+    app.use(express.static('../../../frontend/build'));
     app.get("/", (req, res) => {
         res.sendFile(path.resolve(__dirname, '../../../', 'frontend', 'build', 'index.html'))
     })
